d72e4ae85a749bf5f20b89731ed4c26445c10f13
User <User@LAB3-3>
2024-01-18 14:34:31 +0200

    766tttt

--- NodeInserts/Program.cs
+++ NodeInserts/Program.cs
@@ -1,6 +1,8 @@
 ﻿using NodeClass;
+using NodeInserts.Models;
 using System.ComponentModel.Design;
 using System.Runtime.InteropServices;
+using NodeInserts.Models;
 
 namespace NodeInserts
 {
@@ -52,11 +54,54 @@ namespace NodeInserts
             }
             return countk;
         }
+        public static bool IsThere(Node<ShabatRecievers> node, int day, int month, int year)
+        {
+            Node<ShabatRecievers> p = node;
+            while(p != null)
+            {
+                if(p.GetValue().GetDay()==day&& p.GetValue().GetMonth() == month&& p.GetValue().GetYear()==year)
+                {
+                    return true;
+                }
+                p=p.GetNext();
+            }
+            return false;
+
+        }
+        public static Node<ShabatRecievers> Sick(string name, Node<ShabatRecievers> node)
+        {
+            if (node == null)
+                return null;
+            Node<ShabatRecievers> p = node;
+
+            while (p != null)
+            {
+                if (p.GetValue().GetParent1() == name)
+                    NodeHelper.Delete(p, p.GetValue());        
+                if (p.GetValue().GetParent2() == name)
+                    NodeHelper.Delete(p, p.GetValue());
+                p = p.GetNext();
+            }
+            return node;
+
+
+        }
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello, World!");
             Node<int> node= new Node<int>(4,new Node<int>(5, new Node<int>(6,new Node<int>(4, new Node<int>(4, new Node<int>(4, new Node<int>(22, new Node<int>(3))))))));
             Console.WriteLine(Func1(node, 4));
+
+            Node<ShabatRecievers> shabatShalom = new Node<ShabatRecievers>(new ShabatRecievers("Shiri","Shira",18,1,2024));
+            Node<ShabatRecievers> nextShabat = new Node<ShabatRecievers>(new ShabatRecievers("Ofek", "Aviv", 25, 1, 2024));
+            shabatShalom.SetNext(nextShabat);
+
+            ShabatRecievers sh = shabatShalom.GetValue();
+            string p1 = sh.GetParent1();
+
+
+
         }
     }
 }
